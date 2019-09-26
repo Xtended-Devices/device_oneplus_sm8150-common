f52be2c9b816c5587d02bcc4993c45e280bfbb4e
Tharaga Balachandran <tbalacha@codeaurora.org>
2019-09-26 19:17:58 -0400

    sm8150-common: gralloc: Upgrade mapper and allocators to v3.0
    CRs-Fixed: 2445393
    Change-Id: I8c44ab63ca81523bb6acb75e8ae878cc7466e139

--- common.mk
+++ common.mk
@@ -256,7 +256,8 @@ PRODUCT_PACKAGES += \
     vendor.display.config@2.0 \
     vendor.qti.hardware.display.allocator-service \
     vendor.qti.hardware.display.allocator@1.0-service \
-    vendor.qti.hardware.display.allocator@1.0.vendor
+    vendor.qti.hardware.display.allocator@1.0.vendor \
+    vendor.qti.hardware.display.mapper@3.0
 
 # DRM
 PRODUCT_PACKAGES += \
