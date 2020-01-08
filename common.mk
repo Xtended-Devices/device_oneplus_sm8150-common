2c8f18c8a87a17cc9686ef2edbd3fab7bb42b1b5
Tharaga Balachandran <tbalacha@codeaurora.org>
2020-01-08 17:17:56 -0500

    sm8150-common: gralloc: Add support for IMapper/IAllocator 4.0
    * Add QtiMapper and QtiAllocator 4.0
    * Implement IMapper 4.0 APIs: get, set,
      getFromBufferDescriptorInfo, flushLockedBuffer,
      rereadLockedBuffer, listSupportedMetadataTypes,
      dumpBuffer, dumpBuffers, and getReservedRegion
    
    CRs-Fixed: 2633272
    Change-Id: Ica42e485e426b366ffb41f30f1ea2cf395901356
    Signed-off-by: Baldev Sahu <bsahu@codeaurora.org>

--- common.mk
+++ common.mk
@@ -257,7 +257,8 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.display.allocator-service \
     vendor.qti.hardware.display.allocator@1.0-service \
     vendor.qti.hardware.display.allocator@1.0.vendor \
-    vendor.qti.hardware.display.mapper@3.0
+    vendor.qti.hardware.display.mapper@3.0 \
+    vendor.qti.hardware.display.mapper@4.0.vendor
 
 # DRM
 PRODUCT_PACKAGES += \
