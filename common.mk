973515709edb8c5dc838f9de35545d74eb1bd924
mukesh22584 <mks22584@gmail.com>
2022-10-22 15:55:15 +0530

    sm8150-common: Setup doze overlay

--- common.mk
+++ common.mk
@@ -305,7 +305,8 @@ PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_PACKAGES += \
     CarrierConfigResCommon \
     FrameworksResTarget \
-    OPlusFrameworksResCommon
+    OPlusFrameworksResCommon \
+    OplusDozeResTarget
 
 # Power
 PRODUCT_PACKAGES += \
