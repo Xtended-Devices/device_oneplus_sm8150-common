afdbb8d0943cf98a2c8d0b5519e5144b7699f1ab
ShevT <evgeny.shishkov@gmail.com>
2022-11-07 11:57:08 +0300

    sm8150-common: Enable split notification shade
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- common.mk
+++ common.mk
@@ -304,6 +304,7 @@ PRODUCT_PACKAGES += \
     FrameworksResTarget \
     OPlusFrameworksResCommon \
     OPlusExtrasResCommon \
+    OPlusSystemUIResCommon \
     XtendedFrameworksResCommon \
     XtendedSettingsResCommon \
     XtendedSystemUIResCommon
